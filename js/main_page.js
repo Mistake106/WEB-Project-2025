72bc17371be63b94ce68becd485b33154f29e402
Mistake106 <tankotankov1@gmail.com>
2025-12-28 22:50:06 +0200

    More work on the login popup. Needs more styling.

--- js/main_page.js
+++ js/main_page.js
@@ -1,15 +1,9 @@
-  const openLoginBtn = document.getElementById("Open_Login_Button");
-  const closeLoginBtn = document.getElementById("Close_Login_Button");
-  const overlay = document.getElementById("Login_Popup");
+  const openLoginBtn = document.getElementById("Open_Login_Popup");
+  const overlay = document.getElementById("overlay");
 
-  openBtn.addEventListener("click", () => {
+  openLoginBtn.addEventListener("click", () => {
     overlay.style.display = "flex";
   });
-
-  closeBtn.addEventListener("click", () => {
-    overlay.style.display = "none";
-  });
-
   // Close when clicking outside popup
   overlay.addEventListener("click", (e) => {
     if (e.target === overlay) {
